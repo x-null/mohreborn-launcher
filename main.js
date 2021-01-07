8bd76167790aa895e32252af8288322f16d011d6
Appelpitje <>
2021-01-07 22:00:10 +0100

    add fontawesome + ui changes

--- main.js
+++ main.js
@@ -18,7 +18,9 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 500,
+    minWidth: 800,
+    minHeight: 500,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
