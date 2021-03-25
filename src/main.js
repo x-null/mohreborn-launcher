7d3335c2c2598b1618be5fc40a0eee025142bbd3
Laurens <laurens.depaepe@gmail.com>
2021-03-25 14:47:31 +0100

    fix old files/bugs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+// import store from './store';
 import './index.css';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(router).mount('#app');
+// createApp(App).use(store).use(router).mount('#app');
 
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
 module.exports  = {
-    publicPath: process.env.NODE_ENV  ===  'production'  ?  './'  :  '/',
-    pluginOptions: {
-        electronBuilder: {
-            preload: 'src/preload.ts',
-        }
-    }
+    publicPath: process.env.NODE_ENV  ===  'production'  ?  './'  :  '/'
 }
